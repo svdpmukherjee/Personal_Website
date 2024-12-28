dec99698c0a4b98fe65a8f59e6c81164e6d02120
svdpmukherjee <100602182+svdpmukherjee@users.noreply.github.com>
2024-12-28 18:42:57 +0100

    hero updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
     "Technical Skills": [
       "Research Prototyping & Development",
       "Data Analytics (Quantitative & Qualitative)",
-      "Interactive Data Visualization & Technical Reporting",
+      "Interactive Data Viz & Scientific Reporting",
     ],
   };
 
